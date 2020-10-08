39aa82cc377a4bf1e92dadff1950573f11743dd0
Bora Baykar <borabaykar@gmail.com>
2020-10-08 23:42:47 +0300

    Delete installation script as it is no longer needed

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#TODO: Add logic to directory creation
-echo "Creating directory as it doesn't exist"
-mkdir /usr/share/backgrounds/catalina/
-
-echo "Copying wallpapers and xml"
-cp ./wallpapers/* /usr/share/backgrounds/catalina
-cp ./xml/catalina-timed.xml /usr/share/backgrounds/catalina
